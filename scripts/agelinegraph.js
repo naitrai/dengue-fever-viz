816b0983b53eb109d90f55ade10d041a839d4ab2
ninhf <ninhf@wwu.edu>
2018-12-05 23:49:44 -0800

    hehehe got the time line to pop up hehehe

--- scripts/agelinegraph.js
+++ scripts/agelinegraph.js
@@ -19,23 +19,59 @@ Plotly.d3.csv(
         }
 
         //init variables
-        var width = 120,
-            height = 320,
-            allAges = unpack(rows, "Freq"),
+        var allAges = unpack(rows, "Freq"),
             listOfAges = [],
             listOfFreqs = [];
-
+        var count = 0;
         //getting list of ages from data
         for (var i = 0; i < allAges.length; i++) {
-            listOfFreqs[i].push(allAges[i]);
-            listOfAges[i].push(i + 1);
+            if (allAges[i] != null){
+                listOfFreqs.push(allAges[i]);
+                listOfAges.push(i+1);
+            }
         }
 
+        console.log(listOfAges.length);
 
-        function setLines(chosenYear, y1, y2) {
+        function setLines(listOfAges, listOfFreqs) {
+           var xmin =  Math.min.apply(null, listOfAges);
+           var ymin =  Math.min.apply(null, listOfFreqs);
+           var xmax =  Math.max.apply(null, listOfAges);
+           var ymax =  Math.max.apply(null, listOfFreqs);
+           
+            var line = {
+                type: 'scatter',
+                x: listOfAges,
+                y: listOfFreqs,
+                mode: 'lines',
+                name: 'Red',
+                line: {
+                    color: 'rgb(219, 64, 82)',
+                    width: 3
+                }
+            }
+            var layout = {
+                title: 'Age Frequency Line Chart',
+                width: 800,
+                height: 400,
+                xaxis: {
+                    range: [xmin, xmax],
+                    title: "Ages",
+                    tick0: xmin,
 
-            Plotly.newPlot('line', data, layout);
+                },
+                yaxis: {
+                    range: [ymin, ymax],
+                    title: "Frequency",
+                    tick0: ymin,
+
+                }
+            };
+            console.log(xmin, xmax);
+            var data = [line];
+
+            // Plotly.newPlot('ageline', data, layout);
         }
-
+        setLines(listOfAges, listOfFreqs);
     }
 );
